24441ce72b99e27da7044b49e37e4b2a66b4085c
rafguevara14 <rafaelg@sfu.ca>
2024-12-17 18:48:44 -0800

    clean main

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,3 @@
-
 #include <avr/interrupt.h>
 #include <avr/io.h>
 #include <string.h>
@@ -16,11 +15,7 @@ int main()
 {
     Timer8* timer = nullptr;
     init_timer(timer, TIM2_BASE, clock_select2::PRESCALE_1024);
-
-
     register_irq(isr_index::TIMER2_OVRFLW, [](){});
-
-    // translate dev board pinout to chip pinout
     enable_interrupts();
 
     init_terminal();
